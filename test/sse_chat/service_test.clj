bebf64eb6704f7f0ac1e2a24e17272b09d02c99d
Gabriel Horner <gabriel.horner@gmail.com>
2013-04-05 07:53:59 -0400

    generate service

---
+++ test/sse_chat/service_test.clj
@@ -0,0 +1,18 @@
+(ns sse-chat.service-test
+  (:require [clojure.test :refer :all]
+            [io.pedestal.service.test :refer :all]
+            [io.pedestal.service.http :as bootstrap]
+            [sse-chat.service :as service]))
+
+(def service
+  (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
+
+(deftest home-page-test
+  (is (=
+       (:body (response-for service :get "/"))
+       "Hello World!")))
+
+(deftest about-page-test
+  (is (.contains
+       (:body (response-for service :get "/about"))
+       "Clojure 1.5")))
